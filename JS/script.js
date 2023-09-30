713d1b9cc74a6fdbc116480e0f9009a46c68c742
punit <punitvishwakarma47@gmail.com>
2023-10-01 01:04:52 +0530

    final project

---
+++ JS/script.js
@@ -0,0 +1,78 @@
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
+
+
+// menuIcon.onclick = () => {
+//     menuIcon. classList.toggle('bx-x');
+//     navbar .classList.toggle('active');
+// };
+
+// const mobile_nav = document.querySelector(".mobile-navbar-btn");
+// const nav_header = document.querySelector(".header");
+
+// const toggleleNavbar = () => {
+//     nav_header.classList.toggle("active");
+// };
+
+// alert("plz Subscribe ")
+
+// mobile_nav.addEventListener('click', () => toggleleNavbar ());
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+
+// ScrollReveal({
+//     reset: true,
+//     distance: '80px',
+//     duration: 2000,
+//     delay: 300
+// }); 
+
+// ScrollReveal().reveal('.mynav');
+
+// //Heaing strat
+// ScrollReveal().reveal('.heading', { origin:'top' });
+
+// // section One strat  To-img 
+// ScrollReveal().reveal('.to-img', { origin:'left' });
+// ScrollReveal().reveal('#img-to', { origin:'right' });
+
+// // section 2 strats 
+// ScrollReveal().reveal('.text-bg', { origin:'top' });
+// ScrollReveal().reveal('.team-text', { origin:'buttom' });
+
+// // section 3 strats gallery 
+// ScrollReveal().reveal('.gallery-img', { origin:'left' });
+// ScrollReveal().reveal('.gallery-second-img', { origin:'right' });
+
+// // section 4 strats pricing-text 
+// ScrollReveal().reveal('.pricing-main', { origin:'top' });
+// ScrollReveal().reveal('.pricing-text', { origin:'left' });
+
+// // section 5 About strat
+// ScrollReveal().reveal('.about-main', { origin:'right' });
+
+// // section 6 from blog strats 
+// ScrollReveal().reveal('.img-box', { origin:'left' });
+
+// // section 8 Vide ka icon  strat
+// ScrollReveal().reveal('.icon', { origin:'left' });
+
+// // footer  strat 
+// ScrollReveal().reveal('.footer-main', { origin:'right' });
